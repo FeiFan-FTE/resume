1f17ed01dad5c005f62e1e5de165b3ddeb7e5eef
chenfeifan <cff@uenit.cn>
2020-07-23 15:27:57 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+     <Row>
+        <Col span="12">col-12</Col>
+        <Col span="12">col-12</Col>
+    </Row>
+    <br>
+    <Row>
+        <Col span="8">col-8</Col>
+        <Col span="8">col-8</Col>
+        <Col span="8">col-8</Col>
+    </Row>
+    <br>
+    <Row>
+        <Col span="6">col-6</Col>
+        <Col span="6">col-6</Col>
+        <Col span="6">col-6</Col>
+        <Col span="6">col-6</Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style scoped  lang="less">
+@import "../../src/less/base.less";
+
+*{
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  color: @base;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+	//_c -> src/compoents
+import HelloWorld from '_c/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
